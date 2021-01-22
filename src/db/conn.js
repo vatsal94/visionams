b17d5e2424165a52c0a42b27d68f37e69618bab9
Sagar Chavda <sagarchavda@mobifyi.com>
2021-01-22 17:47:46 +0530

    update package.json & server.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://root:ZakD0iMDuPmOIeuU@cluster0.yoleh.mongodb.net/Cluster0?retryWrites=true&w=majority',{
+/*const mongoose = require('mongoose')
+mongoose.connect('',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
     useCreateIndex:true
@@ -8,11 +8,12 @@ mongoose.connect('mongodb+srv://root:ZakD0iMDuPmOIeuU@cluster0.yoleh.mongodb.net
 }).catch((e) => {
     console.log("Database Connection Error")
 })
-/*
+*/
 const MongoClient = require('mongodb').MongoClient;
-const uri = "";
+const uri = "mongodb+srv://root:ZakD0iMDuPmOIeuU@cluster0.yoleh.mongodb.net/Cluster0?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
+    console.log("Database Connection Successfully")
   const collection = client.db("test").collection("devices");
   client.close();
-});*/
+});
