f9a4cf0e2cf42cdce2dc1d66b0494d262e103337
Sagar Chavda <sagarchavda@mobifyi.com>
2021-01-22 17:18:46 +0530

    update package.json & server.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+/*const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/AMS',{
     useNewUrlParser:true,
     useUnifiedTopology: true,
@@ -7,4 +7,11 @@ mongoose.connect('mongodb://localhost:27017/AMS',{
     console.log("Database Connection Successfully....")
 }).catch((e) => {
     console.log("Database Connection Error")
-})
+})*/
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:root@cluster0.yoleh.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
