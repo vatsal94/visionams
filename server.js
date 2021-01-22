f8b1316892e41702601a69b0f54c358530c8ea90
Sagar Chavda <sagarchavda@mobifyi.com>
2021-01-22 16:33:21 +0530

    node first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'templates/views'))
+hbs.registerPartials(path.join(__dirname, 'templates/partials'))
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/Register', (req, res) => {
+    res.render("register")
+})
+
+app.get('/Login', (req,res) => {
+    res.render("login")
+})
+
+app.listen(8080)
